8c9b4ce7d077d1fd224c2efd3fa7164615c41271
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-10-17 20:01:06 -0500

    Want flags, haven't used them before, no time to test. Todo: Test.

--- go/portScanner.go
+++ go/portScanner.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	fmt.Println(os.Args[1])
+	addPtr := flag.String("a", "127.0.0.1", "Address(es) for scanning. Defaults to loopback.")
+	portRngPtr := flag.String("p", "1,65535", "Port(s) range for scanning. Defaults to 1,65535 == starting at 1, ending at 65535")
+	if len(os.Args) > 1 {
+		fmt.Println(os.Args)
+	}
 }
