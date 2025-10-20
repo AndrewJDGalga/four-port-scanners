283f935387bc1eb995f5476a73f251f99a76ba91
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-10-20 12:59:13 -0500

    PSv5 limit encountered: Parallel not supported. PS Project goal: Experience standard PS. Standard PS locked to 5.1, perforce of desired program unacceptable, alternative approach required.

--- ps/portScanner.ps1
+++ ps/portScanner.ps1
@@ -22,22 +22,39 @@ $portType = [PortOperationTypes]::PortErr
 $targetPorts = $portsStr.Split(",")
 $targetAddrs = $addrsStr.Split(",")
 
+function Scan-AddressPortsAsync {
+    $successAddress = "127.0.0.1"
+    #$failAddress = "172.16.10.22"
+    $ports = 1..65535
+
+    $ports | Foreach-Object -Parallel {
+        param($port, $successAddress)
+        try {
+            $socket = New-Object System.Net.Sockets.TcpClient($successAddress, $port)
+            if($socket.Connected){
+                "Port open: $port"
+                $socket.Close()
+            }
+        } catch [System.Net.Sockets.SocketException] { "Skipping $port"}
+    } -ArgumentList $_, $successAddress -ThrottleLimit 5
+}
 
 function Scan-AddressPorts {
     $successAddress = "127.0.0.1"
     $failAddress = "172.16.10.22"
     #port0 appears typically ignored?
     for($port = 1; $port -le 65535; $port++){
-        if (Test-Connection -Quiet -Count 1 -ComputerName $successAddress){
+        #if (Test-Connection -Quiet -Count 1 -ComputerName $successAddress){
             try {
                 $socket = New-Object System.Net.Sockets.TcpClient($successAddress, $port)
                 if($socket.Connected){
                     "Port open: $port"
                     $socket.Close()
                 }
-            } catch { "Skipping $port"}
-        }
+            } catch [System.Net.Sockets.SocketException] { "Skipping $port"}
+        #}
     }
 }
+
 Write-Host "Scanning..."
-Scan-AddressPorts
+Scan-AddressPortsAsync
