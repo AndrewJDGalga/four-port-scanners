cc85972f7826697faa147046abf548425323f87a
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-10-21 18:33:57 -0500

    cleanout unsuable async, start Job approach (of 3 options, seems starting point)

--- ps/portScanner.ps1
+++ ps/portScanner.ps1
@@ -22,21 +22,8 @@ $portType = [PortOperationTypes]::PortErr
 $targetPorts = $portsStr.Split(",")
 $targetAddrs = $addrsStr.Split(",")
 
-function Scan-AddressPortsAsync {
-    $successAddress = "127.0.0.1"
-    #$failAddress = "172.16.10.22"
-    $ports = 1..65535
-
-    $ports | Foreach-Object -Parallel {
-        param($port, $successAddress)
-        try {
-            $socket = New-Object System.Net.Sockets.TcpClient($successAddress, $port)
-            if($socket.Connected){
-                "Port open: $port"
-                $socket.Close()
-            }
-        } catch [System.Net.Sockets.SocketException] { "Skipping $port"}
-    } -ArgumentList $_, $successAddress -ThrottleLimit 5
+function Scan-PortsJob {
+    
 }
 
 function Scan-AddressPorts {
@@ -44,17 +31,14 @@ function Scan-AddressPorts {
     $failAddress = "172.16.10.22"
     #port0 appears typically ignored?
     for($port = 1; $port -le 65535; $port++){
-        #if (Test-Connection -Quiet -Count 1 -ComputerName $successAddress){
-            try {
-                $socket = New-Object System.Net.Sockets.TcpClient($successAddress, $port)
-                if($socket.Connected){
-                    "Port open: $port"
-                    $socket.Close()
-                }
-            } catch [System.Net.Sockets.SocketException] { "Skipping $port"}
-        #}
+        try {
+            $socket = New-Object System.Net.Sockets.TcpClient($successAddress, $port)
+            if($socket.Connected){
+                "Port open: $port"
+                $socket.Close()
+            }
+        } catch [System.Net.Sockets.SocketException] { "Skipping $port"}
     }
 }
 
 Write-Host "Scanning..."
-Scan-AddressPortsAsync
