6234a0dab02126bb4056c47bd52e1108ce5abb81
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-10-21 22:12:16 -0500

    Failed goscan on DialTimeout Test. Commenting code for removal-happy plugin I hate but keep idk y.

--- go/portScanner.go
+++ go/portScanner.go
@@ -1,15 +1,34 @@
 package main
 
 import (
-	"flag"
+	/*
+		"flag"
+		"os"
+	*/
 	"fmt"
-	"os"
+	"net"
+	"time"
 )
 
 func main() {
-	addPtr := flag.String("a", "127.0.0.1", "Address(es) for scanning. Defaults to loopback.")
-	portRngPtr := flag.String("p", "1,65535", "Port(s) range for scanning. Defaults to 1,65535 == starting at 1, ending at 65535")
-	if len(os.Args) > 1 {
-		fmt.Println(os.Args)
+	/*
+		addPtr := flag.String("a", "127.0.0.1", "Address(es) for scanning. Defaults to loopback.")
+		portRngPtr := flag.String("p", "1,65535", "Port(s) range for scanning. Defaults to 1,65535 == starting at 1, ending at 65535")
+		if len(os.Args) > 1 {
+			fmt.Println(os.Args)
+		}
+	*/
+	/*
+		for i := 1; i < 65535; i++ {
+
+		}
+	*/
+
+	conn, err := net.DialTimeout("tcp", "127.0.0.1:80", time.Second)
+	if err == nil {
+		conn.Close()
+		fmt.Printf("%d Open\n", 80)
+	} else {
+		fmt.Println(err)
 	}
 }
