a6ba6f017a89b8c749c44ff15ead45d5b323c4fd
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-10-23 16:56:13 -0500

    knowledge issue--opError allows for same operations returning diff Err. Not sure why exactly, result of err interface limitation?

--- go/portScanner.go
+++ go/portScanner.go
@@ -15,6 +15,12 @@ func main() {
 			fmt.Println(os.Args)
 		}
 	*/
+	singlePortScanErr("172.16.24.24:445", time.Second, 445) //dial tcp 172.16.24.24:445: i/o timeout
+	singlePortScanErr("127.0.0.1:80", time.Second, 80)
+	//singlePortScan("127.0.0.1:80", time.Second, 80)      //dial tcp 127.0.0.1:80: connectex: No connection could be made because the target machine actively refused it.
+}
+
+func scanPorts() {
 	var wg sync.WaitGroup
 	for i := 1; i < 65535; i++ {
 		wg.Go(func() {
@@ -27,14 +33,25 @@ func main() {
 	wg.Wait()
 }
 
+func singlePortScanErr(addrAndPort string, duration time.Duration, port int) {
+	_, err := net.DialTimeout("tcp", addrAndPort, duration)
+	if err != nil {
+		if opError, ok := err.(*net.OpError); ok {
+			fmt.Println(opError.Err)
+		}
+
+	}
+}
+
 func singlePortScan(addrAndPort string, duration time.Duration, port int) {
 	conn, err := net.DialTimeout("tcp", addrAndPort, duration)
 	if err == nil {
 		conn.Close()
 		fmt.Printf("%d Open\n", port)
 	} else {
+		fmt.Printf("Error: '%v'\nOf type '%T'\n", err, err)
+
 		if conn != nil {
-			fmt.Println(err)
 			conn.Close()
 		}
 	}
