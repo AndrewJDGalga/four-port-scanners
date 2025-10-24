6b9973a5c27e80b218b0ad74432a5f7be48b18e7
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-10-23 20:26:26 -0500

    ugly remaking singlePortScan for return bool, err. Too many ifs, not sure how go-ish.

--- go/portScanner.go
+++ go/portScanner.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 	"sync"
 	"time"
 )
@@ -15,16 +16,24 @@ func main() {
 			fmt.Println(os.Args)
 		}
 	*/
-	singlePortScanErr("172.16.24.24:445", time.Second, 445) //dial tcp 172.16.24.24:445: i/o timeout
-	singlePortScanErr("127.0.0.1:80", time.Second, 80)
+	//singlePortScanErr("172.16.24.24:445", time.Second, 445) //dial tcp 172.16.24.24:445: i/o timeout
+	//singlePortScanErr("127.0.0.1:80", time.Second, 80)
 	//singlePortScan("127.0.0.1:80", time.Second, 80)      //dial tcp 127.0.0.1:80: connectex: No connection could be made because the target machine actively refused it.
+	_, err := singlePortScan("127.0.0.1:80", "tcp", time.Second)
+	if err != nil {
+		fmt.Println(err)
+	}
+	_, err = singlePortScan("172.16.24.24:445", "tcp", time.Second)
+	if err == nil {
+		fmt.Println("no error")
+	}
 }
 
 func scanPorts() {
 	var wg sync.WaitGroup
 	for i := 1; i < 65535; i++ {
 		wg.Go(func() {
-			singlePortScan(fmt.Sprintf("127.0.0.1:%d", i), time.Second, i)
+			//singlePortScan(fmt.Sprintf("127.0.0.1:%d", i), time.Second, i)
 		})
 		if i%10 == 0 {
 			wg.Wait()
@@ -53,6 +62,24 @@ func singlePortScanErr(addrAndPort string, duration time.Duration, port int) {
 	}
 }
 
+func singlePortScan(addr string, network string, duration time.Duration) (bool, error) {
+	success := true
+	conn, err := net.DialTimeout(network, addr, duration)
+	if conn != nil {
+		conn.Close()
+	}
+	if err != nil {
+		success = false
+		if opError, ok := err.(*net.OpError); ok {
+			if !strings.Contains(opError.Err.Error(), "conn") {
+				err = nil
+			}
+		}
+	}
+	return success, err
+}
+
+/*
 func singlePortScan(addrAndPort string, duration time.Duration, port int) {
 	conn, err := net.DialTimeout("tcp", addrAndPort, duration)
 	if err == nil {
@@ -66,3 +93,4 @@ func singlePortScan(addrAndPort string, duration time.Duration, port int) {
 		}
 	}
 }
+*/
